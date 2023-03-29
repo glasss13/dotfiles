be4e33049d8e0ff058e36b2b99b915e8398c79dd
Max Glass <glassmax13@gmail.com>
2023-03-29 16:29:16 -0400

    use XDG spec and move configs to .config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,26 @@
 fish_add_path /usr/local/bin
-fish_add_path ~/.cargo/bin
+fish_add_path ~/.config/cargo/bin
 fish_add_path ~/bin
+
+# Set xdg spec environment variables
+set -x XDG_DATA_HOME $HOME/.local/share
+set -x XDG_STATE_HOME $HOME/.local/state
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_CACHE_HOME $HOME/.cache
+
+set -x CARGO_HOME $XDG_DATA_HOME/cargo
+set -x RUSTUP_HOME $XDG_DATA_HOME/rustup
+
+set -x NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/npmrc
+set -x NODE_REPL_HISTORY $XDG_DATA_HOME/node_repl_history
+
+set -x PYTHONSTARTUP $XDG_CONFIG_HOME/python/pythonrc
+
+set -x RTC_CONFIG_FILE $XDG_CONFIG_HOME/rtx/.tool-versions
+set -x DOCKER_CONFIG $XDG_CONFIG_HOME/docker
+set -x GRADLE_USER_HOME $XDG_DATA_HOME/gradle
+set -x LESSHISTFILE $XDG_CACHE_HOME/less/history
+
 
 function fish_greeting
 	echo -e "Hello Max, you are looking rather handsome today"
@@ -14,10 +34,10 @@ function list_java
     echo (/usr/libexec/java_home -V)
 end
 function java20
-    export JAVA_HOME=(/usr/libexec/java_home -v20 -F)
+    set -x JAVA_HOME (/usr/libexec/java_home -v20 -F)
 end
 function java8
-    export JAVA_HOME=(/usr/libexec/java_home -v1.8 -F)
+    set -x JAVA_HOME (/usr/libexec/java_home -v1.8 -F)
 end
 # Set default java version to 20
 java20
