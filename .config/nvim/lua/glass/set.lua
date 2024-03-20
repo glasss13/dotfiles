253a62112adbeed53b76da5e45591b40b1b05d46
Max Glass <glassmax13@gmail.com>
2024-03-20 19:38:41 -0400

    make nvim undodir go to right place

--- .config/nvim/lua/glass/set.lua
+++ .config/nvim/lua/glass/set.lua
@@ -12,13 +12,12 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim .opt.termguicolors = true
+vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
