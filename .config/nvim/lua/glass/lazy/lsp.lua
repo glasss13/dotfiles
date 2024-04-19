d0954b7f85c68e42ccbefe57537f8df85a59f32c
Max Glass <glassmax13@gmail.com>
2024-04-19 01:56:48 -0500

    add global vim, use telescope

--- .config/nvim/lua/glass/lazy/lsp.lua
+++ .config/nvim/lua/glass/lazy/lsp.lua
@@ -20,11 +20,25 @@ return {
                 "tsserver",
                 "clangd",
                 "gopls",
+                "pylsp",
             },
             automatic_installation = true,
             handlers = {
                 function(server_name)
                     require("lspconfig")[server_name].setup {}
+                end,
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup {
+                        settings = {
+                            Lua = {
+                                runtime = { version = "Lua 5.1" },
+                                diagnostics = {
+                                    globals = { "vim" }
+                                }
+                            }
+                        }
+                    }
                 end
             }
         })
@@ -42,7 +56,6 @@ return {
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
                 ['<C-y>'] = cmp.mapping.confirm({ select = true }),
                 ['<C-space>'] = cmp.mapping.complete(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
             }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
